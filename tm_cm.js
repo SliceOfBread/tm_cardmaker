1611a963e0c16aa4e06860a64c5cb8ada74dfc0f
SliceOfBread <SliceOfBread@users.noreply.github.com>
2020-08-06 16:12:24 -0400

    Minor updates

--- tm_cm.js
+++ tm_cm.js
@@ -27,20 +27,45 @@ function w3_close() {
 function addBlock(info) {
   let c = document.getElementById("cmcanvas");
   let ctx = c.getContext("2d");
-  
+
   let cmd = info.split(':');
   switch (cmd[0]) {
     case 'template':
         let pngName = cmd[1] + "_normal.png";
       break;
     case 'production':
-      let img = document.getElementById("prod");
       let sz = 10;
-      for (let x=40; x < 80; x += sz) {
-        for (let y=100; y < 120; y += sz) {
+      let xpos = 40;
+      let ypos = 100;
+      let width = 180;
+      let height = 140;
+      let border = 4;
+      let img = document.getElementById("prod");
+      for (let x=xpos; x < xpos+width; x += sz) {
+        for (let y=ypos; y < ypos+height; y += sz) {
           ctx.drawImage(img,x,y,sz,sz);
         }
       }
+      // inner gradient
+      let my_gradient = ctx.createLinearGradient(0, ypos, 0, ypos + height);
+      my_gradient.addColorStop(0, "#9d6c43");
+      my_gradient.addColorStop(1, "#5a412c");
+      ctx.fillStyle = my_gradient;
+      ctx.fillRect(xpos, ypos+border, width, border); // top
+      ctx.fillRect(xpos, ypos+height-border*2, width, border); // bottom
+      ctx.fillRect(xpos+border, ypos, border, height); // left
+      ctx.fillRect(xpos+width-border*2, ypos, border, height); // right
+
+      // outer gradient
+      my_gradient = ctx.createLinearGradient(0, ypos, 0, ypos + height);
+      my_gradient.addColorStop(0, "#505050");
+      my_gradient.addColorStop(1, "#c0c0c0");
+      ctx.fillStyle = my_gradient;
+      ctx.fillRect(xpos, ypos, width, border);// top
+      ctx.fillRect(xpos, ypos+height-border, width, border); // bottom
+      ctx.fillRect(xpos, ypos, border, height); // left
+      ctx.fillRect(xpos+width-border, ypos, border, height); // right
+
       break;
   
     default:
